373ea09caaafd44d46c8712528d38f32d7a7926c
ansike <984315765@qq.com>
2021-12-12 11:55:11 +0800

    feat: init pro

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+node -v
+
+yarn build
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        这是ask的首页
+        <a href="/blog">前往blog</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
